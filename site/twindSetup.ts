f2c6ca285cbfa3c7145371ca701b0f214a525058
Juho Vepsäläinen <bebraw@gmail.com>
2022-05-17 15:11:05 +0300

    chore: Define primary and secondary colors

--- site/twindSetup.ts
+++ site/twindSetup.ts
@@ -2,7 +2,11 @@ import * as twindColors from "https://cdn.skypack.dev/twind@0.16.16/colors?min";
 import twindTypography from "https://cdn.skypack.dev/@twind/typography@0.0.2?min";
 
 export default {
-  theme: { extend: { colors: twindColors } },
+  theme: {
+    extend: {
+      colors: { ...twindColors, primary: "#09b5c4", secondary: "#d3fffb" },
+    },
+  },
   plugins: {
     ...twindTypography(),
   },
