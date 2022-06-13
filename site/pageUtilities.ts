37fe6d5ee43a7598787ada5247443e64477e8a67
Juho Vepsäläinen <bebraw@gmail.com>
2022-06-13 15:01:05 +0300

    refactor: Update gustwind

--- site/pageUtilities.ts
+++ site/pageUtilities.ts
@@ -1,4 +1,5 @@
 import images from "../.images.json" assert { type: "json" };
+import markdown from "./transforms/markdown.ts";
 
 // This function allows resolving image paths to Cloudflare ids
 // so that you don't have to use ids at templates.
@@ -8,4 +9,4 @@ function resolveImage(input: string) {
   return images[input];
 }
 
-export { resolveImage };
+export { markdown, resolveImage };
